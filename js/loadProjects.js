b36769e05916eb3b3f61830cc0c28ef3ae128c73
Aurélien Robin <111597427+Athalfrid@users.noreply.github.com>
2024-07-12 10:11:05 +0200

    modification js css

--- js/loadProjects.js
+++ js/loadProjects.js
@@ -12,36 +12,45 @@ $(document).ready(function () {
 
     // Boucle à travers chaque projet dans le JSON
     projects.forEach(function (project, index) {
-        // Construction de la structure HTML pour chaque projet
-        projectHtml += `
+      // Construction de la structure HTML pour chaque projet
+      projectHtml += `
           <div class="project-card" data-index="${index}">
             <div class="project-header">
               <h3 class="project-title">${project.title}</h3>
               <span class="project-toggle">&#9660;</span>
             </div>
             <div class="project-details">
-              <p class="project-description"><i>${project.status ? project.status : ""}</i></p>
+              <p class="project-description"><i style="color:white">${
+                project.status ? project.status : ""
+              }</i></p>
               <p class="project-description">${project.description}</p>
-              <p class="project-technologies"><strong>Technos utilisées:</strong><br> ${project.technologies.join(", ")}</p>
+              <p class="project-technologies"><strong>Technos utilisées:</strong><br> ${project.technologies.join(
+                ", "
+              )}</p>
         `;
-      
-        // Vérification de la présence d'images
-        if (project.images && project.images.length > 0) {
-          projectHtml += `
+
+      // Vérification de la présence d'images
+      if (project.images && project.images.length > 0) {
+        projectHtml += `
             <div class="project-images">
               <span class="image-navigation prev">&#9664;</span>
               <img src="${project.images[0]}" alt="Project Image" class="current-image">
               <span class="image-navigation next">&#9654;</span>
             </div>
           `;
-        }
-      
-        projectHtml += `
-              <a href="${project.github}" target="_blank">Voir sur GitHub</a>
+      }
+
+      projectHtml += `
+              <a href="${project.github}" class="github-link" target="_blank">
+                    <svg class="octicon octicon-mark-github" height="16" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.22 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                    </svg>
+                    Voir sur GitHub
+                </a>
             </div>
           </div>
         `;
-      });
+    });
 
     // Ajout du HTML généré dans le conteneur des projets
     projectsContainer.append(projectHtml);
